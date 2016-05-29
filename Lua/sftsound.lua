91e6a86291feb14b9d0907ff083590bd8e69cfe3
jjstecchino <jjstecchino@yahoo.com>
2016-05-29 12:28:57 -0400

    Started to build my pattern file

--- Lua/sftsound.lua
+++ Lua/sftsound.lua
@@ -13,7 +13,7 @@
 --
 --  A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/>.  
 --    
---  Version 2.1.13a2
+--  Version 2.1.17.6
 -- 
 local ArmedModeWavFile = {}
 local FlightModeWavFile = {}
@@ -25,40 +25,40 @@ local fairHdopThreshold = 5
 local poorHdopThreshold = 99
 
 local function init()
-  ArmedModeWavFile[1]="darmed.wav"       -- armed value 0 - disarmed
-  ArmedModeWavFile[2]="armed.wav"        -- armed value 1 - disarmed
+  ArmedModeWavFile[1]="darmed.wav"          -- armed value 0 - disarmed
+  ArmedModeWavFile[2]="armed.wav"           -- armed value 1 - disarmed
 
-  FlightModeWavFile[1]="stblz.wav"       -- flight mode 0 - Stabilize
-  FlightModeWavFile[2]="acro.wav"        -- flight mode 1 - Acro
-  FlightModeWavFile[3]="althld.wav"      -- flight mode 2 - Alt Hold
-  FlightModeWavFile[4]="auto.wav"        -- flight mode 3 - Auto
-  FlightModeWavFile[5]="gidd.wav"        -- flight mode 4 - Guided
-  FlightModeWavFile[6]="loitr.wav"       -- flight mode 5 - Loiter
-  FlightModeWavFile[7]="rtl.wav"         -- flight mode 6 - RTL
-  FlightModeWavFile[8]="crcl.wav"        -- flight mode 7 - Circle
-  FlightModeWavFile[9]="invldmd.wav"     -- flight mode 8 - Invalid Mode
-  FlightModeWavFile[10]="landing.wav"    -- flight mode 9 - Land
-  FlightModeWavFile[11]="optltr.wav"     -- flight mode 10 - Optical Loiter
-  FlightModeWavFile[12]="drft.wav"       -- flight mode 11 - Drift
-  FlightModeWavFile[13]="invldmd.wav"    -- flight mode 12 - Invalid Mode
-  FlightModeWavFile[14]="sprt.wav"       -- flight mode 13 - Sport
-  FlightModeWavFile[15]="flip.wav"       -- flight mode 14 - Flip
-  FlightModeWavFile[16]="auttn.wav"      -- flight mode 15 - Autotune
-  FlightModeWavFile[17]="poshld.wav"     -- flight mode 16 - Position Hold
+  FlightModeWavFile[1]="stblz.wav"          -- flight mode 0 - Stabilize
+  FlightModeWavFile[2]="acro.wav"           -- flight mode 1 - Acro
+  FlightModeWavFile[3]="althld.wav"         -- flight mode 2 - Alt Hold
+  FlightModeWavFile[4]="auto.wav"           -- flight mode 3 - Auto
+  FlightModeWavFile[5]="gidd.wav"           -- flight mode 4 - Guided
+  FlightModeWavFile[6]="loitr.wav"          -- flight mode 5 - Loiter
+  FlightModeWavFile[7]="rtl.wav"            -- flight mode 6 - RTL
+  FlightModeWavFile[8]="crcl.wav"           -- flight mode 7 - Circle
+  FlightModeWavFile[9]="invldmd.wav"        -- flight mode 8 - Invalid Mode
+  FlightModeWavFile[10]="landing.wav"       -- flight mode 9 - Land
+  FlightModeWavFile[11]="optltr.wav"        -- flight mode 10 - Optical Loiter
+  FlightModeWavFile[12]="drft.wav"          -- flight mode 11 - Drift
+  FlightModeWavFile[13]="invldmd.wav"       -- flight mode 12 - Invalid Mode
+  FlightModeWavFile[14]="sprt.wav"          -- flight mode 13 - Sport
+  FlightModeWavFile[15]="flip.wav"          -- flight mode 14 - Flip
+  FlightModeWavFile[16]="auttn.wav"         -- flight mode 15 - Autotune
+  FlightModeWavFile[17]="poshld.wav"        -- flight mode 16 - Position Hold
 
   MessageSeverityWavFile[1]="emrgncyl.wav"  -- 0 - emergency - System is unusable. This is a "panic" condition
-  MessageSeverityWavFile[2]=""      -- 1 - alert 	  - Action should be taken immediately. Indicates error in non-critical systems
+  MessageSeverityWavFile[2]=""              -- 1 - alert 	  - Action should be taken immediately. Indicates error in non-critical systems
   MessageSeverityWavFile[3]="crtcll.wav"    -- 2 - crititcal - Action must be taken immediately. Indicates failure in a primary system
   MessageSeverityWavFile[4]="errorl.wav"    -- 3 - error     - Indicates an error in secondary/redundant systems
   MessageSeverityWavFile[5]="warngl.wav"    -- 4 - warning   - Indicates an warning in secondary/redundant systems
   MessageSeverityWavFile[6]="noticel.wav"   -- 5 - notice    - An unusual event has occured, though not an error condition. This should be investigated for the root cause
-  MessageSeverityWavFile[7]="info.wav"  -- 6 - info      - Normal operational messages. Useful for logging. No action is required for these messages
-  MessageSeverityWavFile[8]="debug.wav"  -- 7 - debug     - Useful non-operational messages that can assist in debugging. These should not occur during normal operation
+  MessageSeverityWavFile[7]="info.wav"      -- 6 - info      - Normal operational messages. Useful for logging. No action is required for these messages
+  MessageSeverityWavFile[8]="debug.wav"     -- 7 - debug     - Useful non-operational messages that can assist in debugging. These should not occur during normal operation
 
-  HdopWavFile[1]="gpsgdl.wav"        -- good hdop - between 0.1 and 2.5
-  HdopWavFile[2]="gpsfrl.wav"        -- fair hdop between 2.6 and 5
-  HdopWavFile[3]="gpsprl.wav"        -- poor hdop between 5.1 and 98.9
-  HdopWavFile[4]="gpsnfx.wav"        -- no GPS fix
+  HdopWavFile[1]="gpsgdl.wav"               -- good hdop - between 0.1 and 2.5
+  HdopWavFile[2]="gpsfrl.wav"               -- fair hdop between 2.6 and 5
+  HdopWavFile[3]="gpsprl.wav"               -- poor hdop between 5.1 and 98.9
+  HdopWavFile[4]="gpsnfx.wav"               -- no GPS fix
   
 end
 
